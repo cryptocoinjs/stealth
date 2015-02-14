248823393e8d4e0a0b0740f6c91fdc72fe9a71ed
JP Richardson <jprichardson@gmail.com>
2015-02-14 11:20:20 -0600

    src/crypto: added hmacSha256()

--- src/crypto.js
+++ src/crypto.js
@@ -10,6 +10,10 @@ function hash160 (buffer) {
   return crypto.createHash('rmd160').update(buffer).digest()
 }
 
+function hmacSha256(buffer) {
+  return crypto.createHmac('sha256', new Buffer([])).update(buffer).digest()
+}
+
 function sha256x2 (buffer) {
   buffer = crypto.createHash('sha256').update(buffer).digest()
   return crypto.createHash('sha256').update(buffer).digest()
@@ -18,5 +22,6 @@ function sha256x2 (buffer) {
 module.exports = {
   BigInteger: BigInteger,
   hash160: hash160,
+  hmacSha256: hmacSha256,
   sha256x2: sha256x2
 }
