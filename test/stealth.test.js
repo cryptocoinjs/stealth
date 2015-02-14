f75ae7455718fdc2c6b4d8fb23aa10a9343985ca
JP Richardson <jprichardson@gmail.com>
2015-02-14 11:17:13 -0600

    test/fixtures: renamed "private" and "public" to "priv" and "pub" respectively.

--- test/stealth.test.js
+++ test/stealth.test.js
@@ -7,10 +7,10 @@ var fixtures = require('./fixtures')
 describe('stealth', function() {
   fixtures.valid.forEach(function(f) {
     describe('toString()', function() {
-      it('should conver to base58-check string', function() {
+      it('should convert to base58-check string', function() {
         var stealth = new Stealth({
-          payloadPubKey: new Buffer(f.receiverPayload.publicKey, 'hex'),
-          scanPubKey: new Buffer(f.receiverScan.publicKey, 'hex')
+          payloadPubKey: new Buffer(f.receiverPayload.pubKey, 'hex'),
+          scanPubKey: new Buffer(f.receiverScan.pubKey, 'hex')
         })
         assert.equal(stealth.toString(), f.address)
       })
@@ -18,9 +18,9 @@ describe('stealth', function() {
 
     describe('fromString()', function() {
       it('should convert from base58-check string to object', function() {
-        var stealth = Stealth.fromString(f.address)
-        assert.equal(stealth.scanPubKey.toString('hex'), f.receiverScan.publicKey)
-        assert.equal(stealth.payloadPubKey.toString('hex'), f.receiverPayload.publicKey)
+        var stealth = Stealth.fromString(f.base58)
+        assert.equal(stealth.scanPubKey.toString('hex'), f.receiverScan.pubKey)
+        assert.equal(stealth.payloadPubKey.toString('hex'), f.receiverPayload.pubKey)
       })
     })
   })
